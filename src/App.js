9021d5084fda67b300138e1bdb4ebe2cc2a2754d
yvasilyeva11031986 <y.vasilyevaHR@gmail.com>
2023-01-16 21:33:01 +0000

    Added bootsrap
    Done++

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <h1>Weather App</h1>
       <footer>
       This project was coded by Yuliia Vasilyeva and is {" "}
@@ -11,6 +12,7 @@ export default function App() {
       open-sourced on GitHub   
       </a>
       </footer>
+      </div>
     </div>
   );
 }
